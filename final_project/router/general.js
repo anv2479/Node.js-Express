7f39f4897aa876a1ad361b1f7e606a0776d01a85
anv2479 <arnel@acvilla.com>
2025-07-31 15:15:12 -0400

    update files

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,17 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Invalid username or password' })
+    }
+    if (users.some(user => user.username === username)) {
+        return res.status(400).json({ message: 'User already exists' })
+    } else {
+        users.push({ username, password })
+        return res.status(200).json({ message: 'User registered successfully' })
+    } 
 });
 
 // Get the book list available in the shop
